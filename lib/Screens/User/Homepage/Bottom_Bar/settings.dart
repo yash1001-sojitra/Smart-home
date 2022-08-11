41d72e153ad1394a430cf71831f833459191dbe9
yash sojitra <97076802+yash1001-sojitra@users.noreply.github.com>
2022-08-11 17:45:57 +0530

    setting ui complete

--- lib/Screens/User/Homepage/Bottom_Bar/settings.dart
+++ lib/Screens/User/Homepage/Bottom_Bar/settings.dart
@@ -1,7 +1,12 @@
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:smarthome/Logic/Services/auth_services/auth_service.dart';
 
+import '../../../../Logic/Modules/userData_model.dart';
 import '../../../Authentication/Auth_Main/authmain.dart';
 
 class SettingDash extends StatefulWidget {
@@ -14,23 +19,135 @@ class SettingDash extends StatefulWidget {
 class _SettingDashState extends State<SettingDash> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  bool isSwitched = true;
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    User user = authService.getcurrentUser();
+    List<UserData> userDataList = [];
+    final userDataListRaw = Provider.of<List<UserData>?>(context);
+    userDataListRaw?.forEach((element) {
+      if (user.uid == element.id) {
+        userDataList.add(element);
+      } else {
+        return null;
+      }
+    });
     return Scaffold(
       appBar: AppBar(
-        leading: GestureDetector(
-            child: const Icon(Icons.signpost_outlined),
-            onTap: () {
-              _signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const AuthMain();
-                  },
+        toolbarHeight: 70,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Padding(
+          padding: EdgeInsets.only(left: 10.0),
+          child: Text(
+            "Settings",
+            style: TextStyle(color: Colors.black, fontSize: 30),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            SettingsGroup(
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: CupertinoIcons.pencil_outline,
+                  iconStyle: IconStyle(),
+                  title: 'Appearance',
+                  subtitle: "Make Smart Home'App yours",
                 ),
-              );
-            }),
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.dark_mode_rounded,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.red,
+                  ),
+                  title: 'Dark mode',
+                  subtitle: "Automatic",
+                  trailing: Switch.adaptive(
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                ),
+              ],
+            ),
+
+            // You can add a settings title
+            SettingsGroup(
+              settingsGroupTitle: "Account",
+              items: [
+                SettingsItem(
+                  onTap: () async {
+                    _signOut();
+                    await Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const AuthMain();
+                        },
+                      ),
+                    );
+                  },
+                  icons: Icons.exit_to_app_rounded,
+                  title: "Sign Out",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: CupertinoIcons.delete_solid,
+                  title: "Delete account",
+                  titleStyle: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SettingsGroup(
+              settingsGroupTitle: "Security",
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.fingerprint,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.green[500],
+                  ),
+                  title: "Use Fingerprint",
+                ),
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.pattern_sharp,
+                  iconStyle: IconStyle(
+                    iconsColor: Colors.white,
+                    withBackground: true,
+                    backgroundColor: Colors.red,
+                  ),
+                  title: "Use PassCode",
+                ),
+              ],
+            ),
+            SettingsGroup(
+              items: [
+                SettingsItem(
+                  onTap: () {},
+                  icons: Icons.info_rounded,
+                  iconStyle: IconStyle(
+                    backgroundColor: Colors.purple,
+                  ),
+                  title: 'About',
+                  subtitle: "Learn more about Smart Home'App",
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
